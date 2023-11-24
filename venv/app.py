10a6af617929c23feb3d896e23888e56f9577720
Andrew Hamilton <athc5p@umsystem.edu>
2023-11-24 12:29:44 -0600

    Completed index.html

--- venv/app.py
+++ venv/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 #import pygal
 
@@ -9,11 +10,19 @@ app.config["DEBUG"] = True
 #flash  the secret key to secure sessions
 app.config['SECRET_KEY'] = 'your secret key'
 
+def read_csv():
+    with open('stocks.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        symbols = [row['Symbol'] for row in reader]
+
+    return symbols
+
 # use flask's app.route decorate to map the url to that function
 @app.route("/")
-
-# function that returns content or string
-def home():
-    return "Hello bitch"
+def index():
+    symbols = read_csv()
+    chartTypes = ['1: Bar', '2: Line']
+    timeSers = ['1: Intraday', '2: Daily', '3: Weekly', '4: Monthly']
+    return render_template('index.html', symbols=symbols, chartTypes=chartTypes, timeSers=timeSers)
 
 app.run(host="0.0.0.0", port=5001)
