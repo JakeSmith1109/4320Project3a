c87145508126eb4e63568b30025e5a213deb4721
Yuli Zhuo <yzhuo1122@icloud.com>
2023-11-25 00:04:34 -0600

    kept adding to app.py

--- venv/app.py
+++ venv/app.py
@@ -1,30 +1,82 @@
-import sqlite3
-import csv
-from flask import Flask, render_template, request, url_for, flash, redirect, abort
-#import pygal
+from flask import Flask, render_template, request
+import re
+import requests
+import pygal
+from datetime import datetime
 
-# make a Flask application object called app
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
-#flash  the secret key to secure sessions
-app.config['SECRET_KEY'] = 'your secret key'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        stonkSymbol = request.form['stonkSymbol']
+        chartType = request.form['chartType']
+        timeSeries = request.form['timeSeries']
+        beginDate = request.form['beginDate']
+        endDate = request.form['endDate']
 
-#read data from csv file
-def read_csv():
-    with open('stocks.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        symbols = [row['Symbol'] for row in reader]
+        # Your existing code for input validation and API calls
+        # ...
+        apiKey = 'BQXXFJR3NUAO20QT'
 
-    return symbols
+        # API call and chart generation
+        chart = generate_chart(stonkSymbol, chartType, timeSeries, beginDate, endDate)
 
-# use flask's app.route decorate to map the url to that function
-@app.route("/")
-def index():
-    #create info for chart type, time series, and symbols
-    symbols = read_csv()
-    chartTypes = ['1: Bar', '2: Line']
-    timeSers = ['1: Intraday', '2: Daily', '3: Weekly', '4: Monthly']
-    return render_template('index.html', symbols=symbols, chartTypes=chartTypes, timeSers=timeSers)
+        return render_template('index.html', chart=chart)
 
-app.run(host="0.0.0.0", port=5001)
+    return render_template('index.html', chart=None)
+
+
+def generate_chart(stonkSymbol, chartType, timeSeries, beginDate, endDate):
+    apiKey = 'JQDCJ9W4UP3840QP'
+
+    # Your existing code for API calls and data processing
+    if timeSeries == "1":
+        # Code for intraday time series
+
+    elif timeSeries == "2":
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stonkSymbol}&outputsize=full&apikey={apiKey}'
+        r = requests.get(url)
+        data = r.json()
+        daily_data = data['Time Series (Daily)']
+        # ...
+
+    elif timeSeries == "3":
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stonkSymbol}&apikey={apiKey}'
+        r = requests.get(url)
+        data = r.json()
+        weekly_data = data['Weekly Time Series']
+        # ...
+
+    elif timeSeries == "4":
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={stonkSymbol}&apikey={apiKey}'
+        r = requests.get(url)
+        data = r.json()
+        monthly_data = data['Monthly Time Series']
+        # ...
+
+    # Your existing code for data filtering and sorting
+    sorted_data = sorted(data.items(), key=lambda x: x[0])
+    filtered_data = [(date, values) for date, values in sorted_data if beginDate <= date <= endDate]
+
+    # Your existing code for chart creation
+    if chartType == "1":
+        chart = pygal.Bar(title=f'Stock Data for {stonkSymbol}: {beginDate} to {endDate}', x_label_rotation=90, show_minor_x_labels=True)
+        chart.x_labels = [date for date, _ in filtered_data]
+        chart.add('Open', [float(values['1. open']) for _, values in filtered_data])
+        chart.add('Close', [float(values['4. close']) for _, values in filtered_data])
+        chart.add('High', [float(values['2. high']) for _, values in filtered_data])
+        chart.add('Low', [float(values['3. low']) for _, values in filtered_data])
+
+    elif chartType == "2":
+        chart = pygal.Line(title=f'Stock Data for {stonkSymbol}: {beginDate} to {endDate}', x_label_rotation=90, show_minor_x_labels=True)
+        chart.x_labels = [date for date, _ in filtered_data]
+        chart.add('Open', [float(values['1. open']) for _, values in filtered_data])
+        chart.add('Close', [float(values['4. close']) for _, values in filtered_data])
+        chart.add('High', [float(values['2. high']) for _, values in filtered_data])
+        chart.add('Low', [float(values['3. low']) for _, values in filtered_data])
+
+    return chart
+
+if __name__ == '__main__':
+    app.run(debug=True)
